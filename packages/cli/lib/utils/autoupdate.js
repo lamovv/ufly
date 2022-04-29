37c6b740b5a4e10c3e806610b80a4aaf56725bca
lamovv <lamomm@gmail.com>
2022-04-29 11:13:26 +0800

    fix(module): compoent pkg file

--- packages/cli/lib/utils/autoupdate.js
+++ packages/cli/lib/utils/autoupdate.js
@@ -1,9 +1,7 @@
-const sh = require('shelljs');
 const path = require('path');
 const chalk = require('chalk');
 const ora = require('ora');
 const updater = require('npm-updater');
-// const exec = require('child_process').exec;
 const execSync = require('child_process').execSync;
 
 const V_LELVEL = {
@@ -35,14 +33,9 @@ async function autoUpdate() {
   if (r && r.type && r.version) {
     if (V_LELVEL[r.type] <= vLevel) {
       const spinner = ora({color: 'green'}).start(`${chalk.magenta(`发现新版本 ${ chalk.red(`${r.name}@${r.version}`)}，自动升级中，请稍候...\n`)}`);
-      execSync(`npm i ${r.name} -g`, { stdio: 'inherit' });
+      execSync(`npm i ${r.name}@${r.version} -g`, { stdio: 'inherit' });
       spinner.succeed(`${chalk.green('升级完成，请重新执行命令: ')}${chalk.yellow(`ufly ${argv.join(' ')}`)}\n`);
       return false;
-      // exec(`npm i ${r.name} -g`, (err, stdout) => {
-      //   if(!err){
-      //     spinner.succeed(`${chalk.green(`更新完成，自动执行命令: ufly ${ process.argv.slice(2).join(' ') }\n`)}`);
-      //   }
-      // })
     }
   }
   
