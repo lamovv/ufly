6ee197befd24119a49054002265f0c1639e9ff20
lamovv <lamovv@gmail.com>
2022-11-06 22:56:48 +0800

    build(antd): 优化依赖

--- packages/tpl-component-react/tpl/.fatherrc.js
+++ packages/tpl-component-react/tpl/.fatherrc.js
@@ -1,20 +1,20 @@
 export default {
   esm: 'rollup',
   cjs: 'rollup',
-  umd: {
-    name: '{{name}}',
-    globals: {
-      react: 'React',
-      'react-dom': 'ReactDOM',
-    },
-  },
+  // umd: {
+  //   name: '{{name}}',
+  //   globals: {
+  //     react: 'React',
+  //     'react-dom': 'ReactDOM',
+  //   },
+  // },
   extraBabelPlugins: [
     [
       'import',
       {
         libraryName: 'antd',
         libraryDirectory: 'es',
-        style: true,
+        style: 'css',
       },
       'antd',
     ],
